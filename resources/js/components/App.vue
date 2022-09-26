48096379b6d2be6e86c14565ba0e33259a68281d
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-26 16:45:13 +0200

    Created Vue components and link Vue with front.js file. Modify webpack.mix.js file for compile front.js and import him in public/js

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>Coming soon..</div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    components: {},
+    data() {
+        return {};
+    },
+};
+</script>
+
+<style lang="scss" scoped></style>
