9dcb400284e967ad138de7d65642f6d9eb00d948
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-28 16:57:50 +0200

    Added posts category

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+
 use App\Models\Post;
+use App\Models\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
+
 
 class PostSeeder extends Seeder
 {
@@ -16,10 +20,12 @@ class PostSeeder extends Seeder
         for($i = 0; $i < 10; $i++){
             $new_post = new Post();
 
+            $categories_ids = Category::pluck('id')->toArray();
+
             $new_post->title = $faker->sentence();
             $new_post->content = $faker->paragraph(2);
             $new_post->image = $faker->word('animals',true);
-            $new_post->category_id = $faker->randomDigit(1,7);
+            $new_post->category_id = Arr::random($categories_ids);
 
             $new_post->save();
         }
