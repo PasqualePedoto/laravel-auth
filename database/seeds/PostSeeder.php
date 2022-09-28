dd2008ed785e347e6884fc583ac001a3accfe481
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-28 16:02:49 +0200

    Reset

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -19,11 +19,7 @@ class PostSeeder extends Seeder
             $new_post->title = $faker->sentence();
             $new_post->content = $faker->paragraph(2);
             $new_post->image = $faker->word('animals',true);
-
-
-            // $new_post->title = "titolo a caso";
-            // $new_post->content = "content a caso";
-            // $new_post->image = "https://media.istockphoto.com/vectors/thumbnail-image-vector-graphic-vector-id1147544807?k=20&m=1147544807&s=612x612&w=0&h=pBhz1dkwsCMq37Udtp9sfxbjaMl27JUapoyYpQm0anc=";
+            $new_post->category_id = $faker->randomDigit(1,7);
 
             $new_post->save();
         }
