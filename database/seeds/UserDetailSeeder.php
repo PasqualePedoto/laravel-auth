85b3fe7c91187153a1aa9e4abf35298eb95d0b0e
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-29 17:31:27 +0200

    Create User Detail Seeder

--- database/seeds/UserDetailSeeder.php
+++ database/seeds/UserDetailSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\UserDetail;
+use App\User;
+use Faker\Generator as Faker;
 
 class UserDetailSeeder extends Seeder
 {
@@ -9,8 +12,19 @@ class UserDetailSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $users = User::all();
+
+        foreach($users as $user){
+            $new_details = new UserDetail();
+            $new_details->user_id = $user->id;
+            $new_details->first_name = $faker->firstName();
+            $new_details->last_name = $faker->lastName();
+            $new_details->year_of_birth = $faker->year();
+            $new_details->address = $faker->address();
+
+            $new_details->save();
+        }
     }
 }
