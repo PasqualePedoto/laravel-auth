cb3b96e84cc4a4e56cb3b26c7bde82945ec3431c
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-09-30 17:00:27 +0200

    Added BONUS

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 
 use App\Models\Post;
+use App\Models\Tag;
 use App\Models\Category;
 use App\User;
 use Faker\Generator as Faker;
@@ -21,6 +22,7 @@ class PostSeeder extends Seeder
         for($i = 0; $i < 10; $i++){
             $new_post = new Post();
 
+            $tag_ids = Tag::pluck('id')->toArray();
             $categories_ids = Category::pluck('id')->toArray();
             $users_ids = User::pluck('id')->toArray();
 
@@ -31,6 +33,14 @@ class PostSeeder extends Seeder
             $new_post->category_id = Arr::random($categories_ids);
 
             $new_post->save();
+
+            $post_tags = [];
+
+            foreach($tag_ids as $tag){
+                if($faker->boolean()) $post_tags[] = $tag;
+            }
+
+            $new_post->tags()->attach($post_tags);
         }
     }
 }
